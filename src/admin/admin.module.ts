430068c2188ae4939673edeadaebad4ec18e4430
aloshfn1-oss <alosh.fn1@gmail.com>
2026-05-10 02:42:08 +0300

    dda

--- src/admin/admin.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { NotificationsGateway } from '../notifications/notifications.gateway';
-
-@Module({ controllers: [AdminController], providers: [AdminService, PrismaService, NotificationsGateway] })
-export class AdminModule {}
--- src/notifications/notifications.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotificationsGateway } from './notifications.gateway';
-import { PrismaService } from '../prisma/prisma.service';
-@Module({ providers: [NotificationsGateway, PrismaService], exports: [NotificationsGateway] })
-export class NotificationsModule {}
